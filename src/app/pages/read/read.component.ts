60a45f267f527ede6169853e09b85b61a3a606ca
Luiz <luizfelipedn57@gmail.com>
2024-05-15 23:08:21 -0300

    Finalizando create, read, update e delete

--- src/app/pages/read/read.component.ts
+++ src/app/pages/read/read.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Bike} from "../../bike/bike";
+import {ActivatedRoute, Router} from "@angular/router";
+import {BikeService} from "../../bike/bike.service";
 
 @Component({
   selector: 'app-read',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule
+    ],
   templateUrl: './read.component.html',
   styleUrl: './read.component.scss'
 })
 export class ReadComponent {
+  bike: Bike = new Bike();
+  idToSearch!: number;
+  submittedSearch: boolean = false;
 
+
+  constructor(
+    private activateRouted: ActivatedRoute,
+    private router: Router,
+    private bikeService: BikeService
+  ) {
+  }
+
+  onSubmitSearch() {
+    this.bikeService.getById(this.idToSearch).subscribe(value => {
+      this.bike = value;
+      this.submittedSearch = true;
+    } , error => {
+      console.log("Erro:", JSON.stringify(error));
+      alert(`Erro ao buscar o dados:${error.error}`);
+    });
+  }
 }
--- src/app/pages/update/update.component.ts
+++ src/app/pages/update/update.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Bike} from '../../bike/bike';
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/core';
+import {BikeService} from "../../bike/bike.service";
+
+
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './update.component.html',
   styleUrl: './update.component.scss'
 })
-export class UpdateComponent {
 
+
+export class UpdateComponent {
+  bike: Bike = new Bike();
+  submittedSearch: boolean = false;
+  idToUpdate!: number;
+
+  constructor(
+    private bikeService: BikeService,
+    private router: Router,
+  ) { }
+
+  onSubmitSearch() {
+    this.bikeService.getById(this.idToUpdate).subscribe(value => {
+      this.bike = value;
+      this.submittedSearch = true;
+    } , error => {
+      console.log("Erro:", JSON.stringify(error));
+      alert(`Erro ao buscar o dados:${error.error}`);
+    });
+  }
+
+  onSubmit() {
+    this.bikeService.save(this.bike)
+      .subscribe(value => {
+        console.log("Salvo:", JSON.stringify(value));
+        this.router.navigate(['']);
+        //alert("Salvo com sucesso!");
+      }, error => {
+        console.log("Erro" + JSON.stringify(error));
+        alert('Erro ao salvar:');
+      });
+  }
 }
